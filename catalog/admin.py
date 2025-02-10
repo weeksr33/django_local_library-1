321b9c4f1437427ae9dc8a3a1f765909f9f0276a
Michael Stewart <stewarmc@jmu.edu>
2025-02-09 22:08:03 -0500

    beginning of part 4

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Author, Genre, Book, BookInstance, Language
+
+admin.site.register(Book)
+admin.site.register(Author)
+admin.site.register(Genre)
+admin.site.register(BookInstance)
+
+# next line because I completed the challenge in the tutorial
+admin.site.register(Language)
