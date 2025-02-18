429163469c4f8893c85510f9dd445c66efbcf381
Michael Stewart <stewarmc@jmu.edu>
2025-02-18 01:06:42 -0500

    challenge this! (completes part 5

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.db.models import Q
 # Create your views here.
 from .models import Book, Author, BookInstance, Genre
 
@@ -16,11 +16,24 @@ def index(request):
     # The 'all()' is implied by default.
     num_authors = Author.objects.count()
 
+    genre_counts = {
+        "children": Genre.objects.filter(name__icontains='children').count(),
+        "fiction": Genre.objects.filter(name__icontains='fiction').count(),
+        "science": Genre.objects.filter(name__icontains='science').count(),
+    }
+
+    # chained filters are "AND"ed together, resulting in the "intersection" or "conjunction", but I wanted
+    # the "OR"ed result (or "disjunction" or "union") so I used the Q object (note the added import above)
+    # https://docs.djangoproject.com/en/5.0/topics/db/queries/#complex-lookups-with-q-objects
+    scien_books_count = Book.objects.filter(Q(title__icontains='scien') | Q(summary__icontains='scien')).count()
+
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'genre_counts': genre_counts,
+        'scien_books_count': scien_books_count,
     }
 
     # Render the HTML template index.html with the data in the context variable
