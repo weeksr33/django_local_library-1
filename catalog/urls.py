5fd493109e073f2d31b7058fb78fa63d3527787b
Michael Stewart <stewarmc@jmu.edu>
2025-02-18 00:45:50 -0500

    before challenge. i was running the server, and found I needed to kill it and then run it again to test at the end of the "what does it look like?" section

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,4 +3,16 @@ from . import views
 
 urlpatterns = [
 
+    # catalog/ — The home (index) page.
+    path('', views.index, name='index'),
+
+    # catalog/books/ — A list of all books.
+    
+    # catalog/authors/ — A list of all authors.
+    
+    # catalog/book/<id> — The detail view for a particular book, with a field primary key of <id> (the default). For example, the URL for the third book added to the list will be /catalog/book/3.
+    
+    # catalog/author/<id> — The detail view for the specific author with a primary key field of <id>. For example, the URL for the 11th author added to the list will be /catalog/author/11.
+
+
 ]
