18e098682f50c1609930dc75156b8411306331c1
Michael Stewart <stewarmc@jmu.edu>
2025-02-25 07:47:24 -0500

    part 6 w/o challenge

--- catalog/urls.py
+++ catalog/urls.py
@@ -7,10 +7,13 @@ urlpatterns = [
     path('', views.index, name='index'),
 
     # catalog/books/ — A list of all books.
-    
+    path('books/', views.BookListView.as_view(), name='books'),
+
     # catalog/authors/ — A list of all authors.
     
+    
     # catalog/book/<id> — The detail view for a particular book, with a field primary key of <id> (the default). For example, the URL for the third book added to the list will be /catalog/book/3.
+    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
     
     # catalog/author/<id> — The detail view for the specific author with a primary key field of <id>. For example, the URL for the 11th author added to the list will be /catalog/author/11.
 
--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.db.models import Q
+
 # Create your views here.
 from .models import Book, Author, BookInstance, Genre
+
 
 def index(request):
     """View function for home page of site."""
@@ -11,30 +13,67 @@ def index(request):
     num_instances = BookInstance.objects.all().count()
 
     # Available books (status = 'a')
-    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_instances_available = BookInstance.objects.filter(status__exact="a").count()
 
     # The 'all()' is implied by default.
     num_authors = Author.objects.count()
 
     genre_counts = {
-        "children": Genre.objects.filter(name__icontains='children').count(),
-        "fiction": Genre.objects.filter(name__icontains='fiction').count(),
-        "science": Genre.objects.filter(name__icontains='science').count(),
+        "children": Genre.objects.filter(name__icontains="children").count(),
+        "fiction": Genre.objects.filter(name__icontains="fiction").count(),
+        "science": Genre.objects.filter(name__icontains="science").count(),
     }
 
     # chained filters are "AND"ed together, resulting in the "intersection" or "conjunction", but I wanted
     # the "OR"ed result (or "disjunction" or "union") so I used the Q object (note the added import above)
     # https://docs.djangoproject.com/en/5.0/topics/db/queries/#complex-lookups-with-q-objects
-    scien_books_count = Book.objects.filter(Q(title__icontains='scien') | Q(summary__icontains='scien')).count()
+    scien_books_count = Book.objects.filter(
+        Q(title__icontains="scien") | Q(summary__icontains="scien")
+    ).count()
 
     context = {
-        'num_books': num_books,
-        'num_instances': num_instances,
-        'num_instances_available': num_instances_available,
-        'num_authors': num_authors,
-        'genre_counts': genre_counts,
-        'scien_books_count': scien_books_count,
+        "num_books": num_books,
+        "num_instances": num_instances,
+        "num_instances_available": num_instances_available,
+        "num_authors": num_authors,
+        "genre_counts": genre_counts,
+        "scien_books_count": scien_books_count,
     }
 
     # Render the HTML template index.html with the data in the context variable
-    return render(request, 'index.html', context=context)
+    return render(request, "index.html", context=context)
+
+
+from django.views import generic
+
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10
+
+    # examples of customizations we don't currently need
+    # context_object_name = 'book_list'   # your own name for the list as a template variable
+    # queryset = Book.objects.filter(title__icontains='war')[:5] # Get 5 books containing the title war
+    # template_name = 'books/my_arbitrary_template_name_list.html'  # Specify your own template name/location
+    #
+    # overriding class methods
+    # def get_queryset(self):
+    #     return Book.objects.filter(title__icontains='war')[:5] # Get 5 books containing the title war
+    #
+    # We might also override get_context_data() in order to pass additional context variables to
+    # the template (e.g. the list of books is passed by default).
+    # The fragment below shows how to add a variable named some_data to the
+    # context (it would then be available as a template variable).
+    # def get_context_data(self, **kwargs):
+    #     # When doing this it is important to follow the pattern used above:
+    #     # First get the existing context from our superclass.
+    #     # Then add your new context information.
+    #     # Then return the new (updated) context.
+    #
+    #     # Call the base implementation first to get the context
+    #     context = super(BookListView, self).get_context_data(**kwargs)
+    #     # Create any data and add it to the context
+    #     context['some_data'] = 'This is just some data'
+    #     return context
+class BookDetailView(generic.DetailView):
+    model = Book
